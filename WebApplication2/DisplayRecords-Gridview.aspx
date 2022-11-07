de893403a538ec113ff7658c4a97553b8e2b2f96
Ravi Gudivada <ravigudivada143@gmail.com>
2022-11-07 15:10:31 +0530

    first commit

---
+++ WebApplication2/DisplayRecords-Gridview.aspx
@@ -0,0 +1,35 @@
+﻿
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                <h1>Search Records from the Database</h1>
+                <h2>Display in a Gridview Control !</h2>
+                <hr />
+            Search :<asp:TextBox ID="Textsearch" runat="server"></asp:TextBox>
+            <asp:Button ID="ButtonSearch" runat="server" Text="Search" OnClick="ButtonSearch_Click"  ForeColor=/>
+            <hr />
+              <asp:GridView ID="GridView1" runat="server" ShowHeaderWhenEmpty="true" EmptyDataText="No Records Found!">
+
+            </asp:GridView>
+                      </center>
+           
+
+          
+                   
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DisplayRecords-Gridview.aspx.cs" Inherits="WebApplication2.DisplayRecords_Gridview" %>
+
+            </script>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/DisplayRecords-Gridview.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Reflection;
+
+namespace WebApplication2
+{
+    public partial class DisplayRecords_Gridview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            String mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand();
+            string sqlquery = "select * from [dbo].[SHG_Master] inner Join[dbo].[SHG_Master_log] on[dbo].[SHG_Master].Shg_ID =[dbo].[SHG_Master_log].Shg_ID where Shg_ID like '%'+Shg_ID+'%'";
+            sqlcomm.CommandText = sqlquery;
+            sqlcomm.Connection = sqlconn;
+            sqlcomm.Parameters.AddWithValue("Shg_ID",Textsearch.Text);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
+            sda.Fill(dt);
+            dt = Con.query;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
